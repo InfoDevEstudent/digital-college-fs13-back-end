bbb85861529360d82ea075b7bd6230e3b1db0fe1
InfoDevEstudent <infodev.infodev@gmail.com>
2023-07-26 00:33:44 -0300

    express inicio aula 03

--- aula03/index.js
+++ aula03/index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 const database = require('./conexao')
 
 async function recepcao(req, res) {
@@ -12,8 +12,42 @@ async function recepcao(req, res) {
         return;
     }
 
+    if (req.url === '/alunos/5')
+
     
     res.end('Olá Sergio')
 }
 
-http.createServer(recepcao).listen(8000)
+http.createServer(recepcao).listen(8000) */
+
+const express = require('express');
+const database = require('./conexao');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.end('Olá Sergio Felipe');
+});
+
+app.get('./alunos', async (req, res) => {
+    let dados = await database.executar('SELECT * FROM tb_aluno');
+
+    /* res.end(
+        JSON.stringify(dados)
+    ); */
+    res.json(dados);
+});
+
+app.get('./alunos/:id', async (req, res) => {
+    let id = req.params.id;
+
+    let dados = await database.executar(
+        `SELECT * FROM tb_aluno WHERE id=${id}`
+    );
+
+    res.json(dados);
+})
+
+app.listen(8000, () => {
+    console.log('API rodando')
+})
